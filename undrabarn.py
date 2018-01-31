a976dd1def8a8810930b9cbac8778bda9b416a3c
Guðni Natan Gunnarsson <1493259+GudniNatan@users.noreply.github.com>
2018-01-31 15:57:37 +0000

    Update undrabarn.py

--- undrabarn.py
+++ undrabarn.py
@@ -1,3 +1,12 @@
+import math
 n = int(raw_input())
 
-print int(str(  (n)).replace("L", ""))
+x = str(oct(n))[1::]
+xOG = x
+for i in range(len(x)-1, -1, -1):
+    if x[i]== '0':
+        x = str(int(int(x) - math.pow(10, (len(x) - i - 1))))
+    elif x[i] == '9' and xOG[i] == '0':
+        x = str(int(int(x) - math.pow(10, (len(x) - i - 1)) * 2))
+
+print x
